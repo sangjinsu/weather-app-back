1de33fb0bac6a536ad7e39262e5c3feb6f49da0d
jinsuSang <climbplant39@kakao.com>
2021-12-10 01:10:13 +0900

    feat: add fetch weather function

--- tools/fetchWeather_test.go
+++ tools/fetchWeather_test.go
@@ -1,22 +1,32 @@
 package tools
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestFetchWeather(t *testing.T) {
+
+	LoadEnv()
+
 	type args struct {
 		latitude  float64
 		longitude float64
-		placeName string
 	}
 	tests := []struct {
-		name string
-		args args
+		name    string
+		args    args
+		wantErr error
 	}{
-		// TODO: Add test cases.
+		{"seoul", args{37.58333, 127}, nil},
+		{"incheon", args{37.46389, 126.64861}, nil},
+		{"london", args{51.507321899999994, -0.12764739999999997}, nil},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			FetchWeather(tt.args.latitude, tt.args.longitude, tt.args.placeName)
+			assertions := assert.New(t)
+			_, _, _, _, _, err := FetchWeather(tt.args.latitude, tt.args.longitude)
+			assertions.ErrorIsf(err, nil, "FetchWeather(%v, %v)", tt.args.latitude, tt.args.longitude)
 		})
 	}
 }
